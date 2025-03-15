a7d91ef949127d8ed6028763940aea5561379de8
Guilherme Torres <guilhermetorres1401@gmail.com>
2025-03-15 10:45:47 -0300

    feature: passagem de prompt com documentos recuperados para o modelo

--- chroma_config.py
+++ chroma_config.py
@@ -1,12 +1,16 @@
+import os
 import chromadb
 from chromadb.utils.embedding_functions.ollama_embedding_function import OllamaEmbeddingFunction
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class ChromaConfig:
     def __init__(self):
-        self.OLLAMA_HOST = 'http://localhost:11434'
-        self.MODEL_NAME = 'llama3.2:3b'
-        self.DATASET_PATH = './data.json'
-        self.COLLECTION_NAME = 'documentos'
+        self.OLLAMA_HOST = os.getenv('OLLAMA_HOST')
+        self.MODEL_NAME = os.getenv('MODEL_NAME')
+        self.DATASET_PATH = os.getenv('DATASET_PATH')
+        self.COLLECTION_NAME = os.getenv('COLLECTION_NAME')
     
     def get_client(self):
         return chromadb.PersistentClient()
--- rag.py
+++ rag.py
@@ -1,7 +1,8 @@
 from chroma_config import ChromaConfig
 from chromadb import ClientAPI
+import ollama
 
-def retrieve_documents(client: ClientAPI, collection_name, embedding_function, query, n=2):
+def retrieve_documents(client: ClientAPI, collection_name, embedding_function, query, n=3):
     collection = client.get_or_create_collection(
         name=collection_name,
         embedding_function=embedding_function
@@ -12,18 +13,38 @@ def retrieve_documents(client: ClientAPI, collection_name, embedding_function, q
     )
     return results
 
-def generate_output():
-    pass
+def generate_output(query: str, documents: list):
+    prompt = f'''
+    ## Instrução ##
+    Você é um assistente especializado que responde perguntas de forma clara e objetiva 
+    com base nos documentos fornecidos. Se a resposta não estiver nos documentos,
+    você deve dizer que não encontrou a informação.
+    ## Documentos ##
+    {'\n'.join([f' - {document}' for document in documents])}
+    ## Pergunta ##
+    {query}
+    Resposta:
+    '''
+    output = ollama.generate(
+        model=ChromaConfig().MODEL_NAME,
+        prompt=prompt,
+        options={
+            'temperature': 0
+        }
+    )
+    return output.response
 
 def main():
-    query = 'Quais são as formas de pagamento?'
-    documents = retrieve_documents(
+    query = 'Como faço para entrar em contato?'
+    result = retrieve_documents(
         client=ChromaConfig().get_client(),
         collection_name=ChromaConfig().COLLECTION_NAME,
         embedding_function=ChromaConfig().get_embedding_function(),
         query=query
     )
-    print(documents)
+    documents = result['documents'][0]
+    output = generate_output(query=query, documents=documents)
+    print(output)
 
 if __name__ == '__main__':
     main()
